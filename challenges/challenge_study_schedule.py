12f1a8dc3c6e060f374af86dbce22ec9e62c608f
gemaquejr <gemaquejr@hotmail.com>
2023-01-25 19:11:57 -0300

    Adiciona algoritimo de busca

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    students = 0
+
+    # Ajuda da Maria Carolina na mentoria
+
+    if target_time:
+        for login, logout in permanence_period:
+            if not type(login) is int or not type(logout) is int:
+                return None
+            if login <= target_time <= logout:
+                students += 1
+        return students
+    return None
