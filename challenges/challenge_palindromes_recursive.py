5a3931a89564c3199a0c5123bd81729bb3a3152b
gemaquejr <gemaquejr@hotmail.com>
2023-01-26 16:56:03 -0300

    Feat Adiciona Referência

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -9,4 +9,6 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word[low_index] != word[high_index]:
         return False
 
+    # Ajuda da Maria Carolina na Mentoria
+
     return is_palindrome_recursive(word, (low_index + 1), (high_index - 1))
