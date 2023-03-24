488b34349a9c60fbb2414830b8451316429832da
gemaquejr <gemaquejr@hotmail.com>
2023-03-24 16:22:35 -0300

    Adiciona teste de encriptacao

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
 # from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("", 5) == ""
+    assert encrypt_message("abcdef", 0) == "fedcba"
+    assert encrypt_message("abcdef", 7) == "fedcba"
+    assert encrypt_message("teste de mensagem", 5) == "etset_megasnem ed "
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("abcdef", "3")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 5)
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(5, 5)
