5cbbf6bb320ae2b62b31ab9949f1060cce356f07
gemaquejr <gemaquejr@hotmail.com>
2023-03-24 16:26:09 -0300

    Adiciona a funcao palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    lengh_of_word = len(word)
+    start = 0
+    end = lengh_of_word - 1
+    while start < end:
+        if word[start] != word[end]:
+            return False
+        start += 1
+        end -= 1
+
+    return True
