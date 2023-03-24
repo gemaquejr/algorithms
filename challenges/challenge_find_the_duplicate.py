6b3bd94f2b2aa43cb84513fe09a1ea31254e4e11
gemaquejr <gemaquejr@hotmail.com>
2023-03-24 16:24:49 -0300

    Adiciona a funcao find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or not all(isinstance(num, int) and num >= 0 for num in nums):
+        return False
+
+    counts = {}
+    for num in nums:
+        if num in counts:
+            return num
+        counts[num] = 1
+    return False
